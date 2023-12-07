cfeb6a29ba37439668c8e8cff3cdd1ae087f7d47
Kevin <128312170+mylordkaz@users.noreply.github.com>
2023-12-07 22:15:55 +0900

    modify code to add Async/await

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 document.addEventListener('DOMContentLoaded', () => {
   const search = document.querySelector('#search');
 
-  search.addEventListener('submit', (event) => {
+  search.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const APIKey = '801c8533cc3e46ae8b2164127230612 ';
@@ -16,23 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('input').value = '';
     }
 
-    fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${input}&aqi=no`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        const details = document.querySelector('.details');
-        const temperature = document.querySelector('.temperature');
-        const humidity = document.querySelector('.humidity');
-        const wind = document.querySelector('.wind');
-        const image = document.querySelector('.image');
-        const iconUrl = `https:${json.current.condition.icon}`;
-        image.src = iconUrl;
-        details.innerHTML = json.current.condition.text;
-        temperature.innerHTML = `${json.current.temp_c}<span>°C</span>`;
-        humidity.innerHTML = `${json.current.humidity}<span> Humidity</span>`;
-        wind.innerHTML = `${json.current.wind_kph}<span> km/h wind speed</span>`;
-        console.log(json);
-      });
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${input}&aqi=no`
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const json = await response.json();
+
+      const details = document.querySelector('.details');
+      const temperature = document.querySelector('.temperature');
+      const humidity = document.querySelector('.humidity');
+      const wind = document.querySelector('.wind');
+      const image = document.querySelector('.image');
+      const iconUrl = `https:${json.current.condition.icon}`;
+
+      image.src = iconUrl;
+      details.innerHTML = json.current.condition.text;
+      temperature.innerHTML = `${json.current.temp_c}<span>°C</span>`;
+      humidity.innerHTML = `${json.current.humidity}<span> Humidity</span>`;
+      wind.innerHTML = `${json.current.wind_kph}<span> km/h wind speed</span>`;
+      console.log(json);
+    } catch (error) {
+      console.error('Error fetchig data', error.message);
+    }
   });
 });
